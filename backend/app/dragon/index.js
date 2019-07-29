b4ad47e388f00bd11453a03c5b26773c50d322fa
mono motapa <bahobab@hotmail.com>
2019-07-29 13:54:31 -0500

    Modified dragon entity: added new fields to the table and updated queries accordingly

--- backend/app/dragon/index.js
+++ backend/app/dragon/index.js
@@ -4,6 +4,8 @@ const DragonTable = require("./table");
 const DEFAULT_PROPERTIES = {
   dragonId: undefined,
   nickname: "undefined",
+  isPublic: false,
+  saleValue: 0,
   generationId: undefined,
 
   get birthdate() {
@@ -26,12 +28,22 @@ const DEFAULT_PROPERTIES = {
 };
 
 class Dragon {
-  constructor({ dragonId, birthdate, nickname, traits, generationId } = {}) {
+  constructor({
+    dragonId,
+    birthdate,
+    nickname,
+    traits,
+    generationId,
+    isPublic,
+    saleValue
+  } = {}) {
     this.dragonId = dragonId || DEFAULT_PROPERTIES.dragonId;
     this.birthdate = birthdate || DEFAULT_PROPERTIES.birthdate;
     this.nickname = nickname || DEFAULT_PROPERTIES.nickname;
     this.traits = traits || DEFAULT_PROPERTIES.randomTrait;
     this.generationId = generationId || DEFAULT_PROPERTIES.generationId;
+    this.isPublic = isPublic || DEFAULT_PROPERTIES.isPublic;
+    this.saleValue = saleValue || DEFAULT_PROPERTIES.saleValue;
   }
 
   buildDragon() {}
--- backend/app/dragon/table.js
+++ backend/app/dragon/table.js
@@ -3,12 +3,14 @@ const DragonTraitTable = require("../dragonTrait/table");
 
 class DragonTable {
   static storeDragon(dragon) {
-    const { birthdate, nickname, generationId } = dragon;
+    const { birthdate, nickname, generationId, isPublic, saleValue } = dragon;
 
     return new Promise((resolve, reject) => {
       pool.query(
-        `INSERT INTO dragon(birthdate, nickname, "generationId") VALUES($1, $2, $3) RETURNING id`,
-        [birthdate, nickname, generationId],
+        `INSERT INTO dragon(birthdate, nickname, "generationId", "isPublic", "saleValue")
+        VALUES($1, $2, $3, $4, $5)
+        RETURNING id`,
+        [birthdate, nickname, generationId, isPublic, saleValue],
         (error, response) => {
           if (error) return reject(error);
 
@@ -39,7 +41,7 @@ class DragonTable {
 
     return new Promise((resolve, reject) => {
       pool.query(
-        `SELECT birthdate, nickname, "generationId"
+        `SELECT birthdate, nickname, "generationId", "isPublic", "saleValue"
                   FROM dragon
                   WHERE dragon.id = $1`,
         [dragonId],
@@ -53,15 +55,15 @@ class DragonTable {
     });
   }
 
-  static updateDragon({ dragonId, nickname }) {
+  static updateDragon({ dragonId, nickname, isPublic, saleValue }) {
     console.log("Table: update nickname", { nickname, dragonId });
 
     return new Promise((resolve, reject) => {
       pool.query(
         `UPDATE dragon
-        SET nickname = $1
-        WHERE id = $2`,
-        [nickname, dragonId],
+        SET nickname = $1, "isPublic" = $2, "saleValue" = $3
+        WHERE id = $4`,
+        [nickname, isPublic, saleValue, dragonId],
         (error, response) => {
           if (error) return reject(error);
 
